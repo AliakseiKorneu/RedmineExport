41859fcda51848abb691f5867587e35d916353ed
AliakseiKorneu <kornev_a@me.com>
2016-01-08 12:14:52 +0300

    Change names of functions in accordance with the Python Code Style Guide

--- export.py
+++ export.py
@@ -4,8 +4,8 @@ import sys
 import argparse
 import re
 
-# Create Parser function. Set arguments and their parametrs: is required, help messages, etc.
-def createParser ():
+# Set arguments and their parametrs: is required, help messages, etc.
+def create_parser ():
 	parser = argparse.ArgumentParser(prog = 'Redmine Export')
 	parser.add_argument ('-f', '--format', choices = ['json','xml'], required = True, help = 'Export Format')
 	parser.add_argument ('-ht', '--host', required = True, help = 'Redmine Host. Format: http(s)://...')
@@ -15,7 +15,7 @@ def createParser ():
 	return parser 
 
 # Check host address. 
-def urlValidator (url):
+def check_host_address (url):
 	regex = re.compile(
 			r'^(?:http|ftp)s?://' 
 			r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' 
@@ -32,11 +32,11 @@ def urlValidator (url):
 # Get issues info.		
 # There is no requirements for the limit of returned projects/issues and for order. Left by default.
 # There is no XML validation. We get data that Redmine returnes.
-def getIssuesInfo (namespace):
-	if (namespace.format == 'xml'):
-		url = namespace.host+'/issues.xml?project_id='+namespace.project
-	if (namespace.format == 'json'):
-		url = namespace.host+'/issues.json?project_id='+namespace.project
+def get_issues_info (arguments):
+	if (arguments.format == 'xml'):
+		url = arguments.host+'/issues.xml?project_id='+arguments.project
+	if (arguments.format == 'json'):
+		url = arguments.host+'/issues.json?project_id='+arguments.project
 	try:
 		response = urllib2.urlopen(url)
 		info = response.read()
@@ -46,11 +46,11 @@ def getIssuesInfo (namespace):
 	return info
 
 # Get projects info.
-def getProjectsInfo (namespace):
-	if (namespace.format == 'xml'):
-		url = namespace.host+'/projects.xml'
-	if (namespace.format == 'json'):
-		url = namespace.host+'/projects.json'
+def get_projects_info (arguments):
+	if (arguments.format == 'xml'):
+		url = arguments.host+'/projects.xml'
+	if (arguments.format == 'json'):
+		url = arguments.host+'/projects.json'
 	try:
 		response = urllib2.urlopen(url)
 		info = response.read()
@@ -59,22 +59,24 @@ def getProjectsInfo (namespace):
 
 	return info
 
+def redmine_export ():
+	parser = create_parser()
+	# Convert arguments to lower case. It allows to enter argements in any case.
+	raw_arguments = map(str.lower,sys.argv[1:])
+	arguments = parser.parse_args(raw_arguments)
+	isValid = check_host_address(arguments.host)
 
-parser = createParser()
-# Convert arguments to lower case. It allows to enter argements in any case.
-arguments = map(str.lower,sys.argv[1:])
-namespace = parser.parse_args(arguments)
-isValid = urlValidator(namespace.host)
+	if (isValid == False):
+		sys.exit('Please, check the format of host address. Format: http(s)://...')
+	# If Issues object is selected, user must enter project ID
+	if (arguments.object == 'issues'):
+		if (arguments.project is None):
+			sys.exit('Please, enter project ID')
+		else:
+			result = get_issues_info(arguments)
+	if (arguments.object == 'projects'):
+		result = get_projects_info(arguments)
 
-if (isValid == False):
-	sys.exit('Please, check the format of host address. Format: http(s)://...')
-# If Issues object is selected, user must enter project ID
-if (namespace.object == 'issues'):
-	if (namespace.project is None):
-		sys.exit('Please, enter project ID')
-	else:
-		result = getIssuesInfo(namespace)
-if (namespace.object == 'projects'):
-	result = getProjectsInfo(namespace)
+	print (result)
 
-print (result)
+redmine_export()
